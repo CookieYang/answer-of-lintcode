96706551f8de1ca3910aaba3eff6e5731f7c225e
cookieYang <yangshifu12138@icloud.com>
2017-08-23 11:30:23 +0800

    binarySearch

--- binarySearch.py
+++ binarySearch.py
@@ -13,17 +13,21 @@ def binarySearch(nums, target):
         return right
     return -1
 
-# def searchInsert(A, target):
-#     left = 0
-#     right = len(A) - 1
-#     while left < right:
-#         mid = int((left + right) / 2)
-#         if A[mid] > target:
-#             right = mid - 1
-#         elif A[mid] < target:
-#             left = mid + 1
-#         else:
-#             return mid
-#     return mid
-#
-# print(searchInsert([1,3,5,6],6))
+def searchInsert(A, target):
+    left = 0
+    right = len(A) - 1
+    if right == -1:
+        return 0
+    while left < right:
+        mid = int((left + right) / 2)
+        if A[mid] > target:
+            right = mid
+        elif A[mid] < target:
+            left = mid + 1
+        else:
+            return mid
+    if  target > A[-1]:
+        return len(A)
+    return right
+
+print(searchInsert([1,3,5,6],7))
