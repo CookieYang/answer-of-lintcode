b9aca7ffa997d2369c8bd64ee09f6d8ffeb49c48
DESKTOP-ABBNTO3\yangtao <1359870821@qq.com>
2017-08-29 20:32:48 +0800

    expressionExpand 递归实现

--- expressionExpand.py
+++ expressionExpand.py
@@ -9,13 +9,20 @@ def helpExpression(s, depth = 0):
     while depth < len(s):
         if s[depth].isdigit():
             repeat = int(s[depth])
+            depth += 1
         elif s[depth] == '[':
-            result = result + helpExpression(s, depth)
+            depth += 1
+            tmp = helpExpression(s, depth)
+            depth += len(tmp) + 1
+            while repeat >= 1:
+                result = result + tmp
+                repeat -= 1
         elif s[depth].isalpha():
             result = result + s[depth]
+            depth += 1
         else:
-            while repeat > 1:
-                result += result
-                repeat -= 1
+            depth += 1
             return  result
+    return result
 
+print(expressionExpand('3[2[ad]3[pf]]xyz'))
