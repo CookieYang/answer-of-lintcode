89ea20d77806ebe7233a78fb7448dcf16bab42c6
cookieYang <yangshifu12138@icloud.com>
2017-11-02 18:01:47 +0800

    打劫房屋II

--- minAdjustmentCost.py
+++ minAdjustmentCost.py
@@ -17,3 +17,22 @@ def MinAdjustmentCost(A, target):
             ans = f[n][i]
 
     return ans
+
+def cutting(prices, n):
+    r = [0]
+    for i in range(1, n + 1):
+        p = -1000
+        for j in range(1, i + 1):
+            p = max(p, prices[j - 1] + r[i - j])
+        r.append(p)
+    return r[n]
+
+def houseRobber(A):
+    cur, last, be_last = 0, 0, 0
+    for i in range(len(A)):
+        cur = max(last, be_last + A[i])
+        be_last = last
+        last = cur
+    return cur
+
+print(cutting([1, 5, 8, 9, 10, 17, 17, 20],8))
