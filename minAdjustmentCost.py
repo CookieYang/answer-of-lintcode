d4702e6246958019ea5e93855f541eedfc5a38c0
cookieYang <yangshifu12138@icloud.com>
2017-11-07 18:00:10 +0800

    快乐数

--- minAdjustmentCost.py
+++ minAdjustmentCost.py
@@ -38,6 +38,22 @@ def houseRobber(A):
 def houseRobberII(A):
     return max(houseRobber(A[1:]),houseRobber(A[:-1]))
 
+def houseRobber3(self, root):
+        # write your code here
+    rob, not_rob = self.visit(root)
+    return max(rob, not_rob)
+
+def visit(self, root):
+    if root is None:
+        return 0, 0
+
+    left_rob, left_not_rob = self.visit(root.left)
+    right_rob, right_not_rob = self.visit(root.right)
+
+    rob = root.val + left_not_rob + right_not_rob
+    not_rob = max(left_rob, left_not_rob) + max(right_rob, right_not_rob)
+    return rob, not_rob
+
 def numWays(n, k):
         # write your code here
     if n == 0:
